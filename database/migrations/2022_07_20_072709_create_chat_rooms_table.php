e700c1eee785a033655229db6dd9514c6c82215e
dennyoctavian <dennyoctavian164@gmail.com>
2022-07-20 14:45:56 +0700

    make migration about design table chat-api

---
+++ database/migrations/2022_07_20_072709_create_chat_rooms_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chat_rooms', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_one');
+            $table->foreign('user_one')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+            $table->unsignedBigInteger('user_two');
+            $table->foreign('user_two')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chat_rooms');
+    }
+};
